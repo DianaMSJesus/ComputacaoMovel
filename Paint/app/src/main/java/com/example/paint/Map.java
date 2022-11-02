d835eac71e2f4e72ca22b2f7105e76ce8e672cfe
DianaMSJesus <83040508+DianaMSJesus@users.noreply.github.com>
2022-11-02 15:14:54 +0000

    Map Update 1
    Já é possivel ver a nossa localização atual

--- Paint/app/src/main/java/com/example/paint/Map.java
+++ Paint/app/src/main/java/com/example/paint/Map.java
@@ -9,14 +9,22 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class Map extends AppCompatActivity implements OnMapReadyCallback {
+
+public class Map extends AppCompatActivity {
 
     SupportMapFragment supportMapFragment;
-    //FusedLocationProviderClient user;
+    FusedLocationProviderClient user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,52 +32,46 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
         setContentView(R.layout.activity_map);
 
         supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
-        //user = LocationServices.getFusedLocationProviderClient(this);
+        user = LocationServices.getFusedLocationProviderClient(this);
 
-        //getCurrentLocation();
-       /* if (ActivityCompat.checkSelfPermission(Map.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            getCurrentLocation();
-        }else{
-            ActivityCompat.requestPermissions(Map.this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION},44);
-        }*/
-
-        supportMapFragment.getMapAsync(this);
-    }
-
-   /* private void getCurrentLocation() {
-        //if (ActivityCompat.checkSelfPermission(Map.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(Map.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             Task<Location> task = user.getLastLocation();
-            task.addOnSuccessListener(new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    if(location != null){
-                        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
-                            @Override
-                            public void onMapReady(@NonNull GoogleMap googleMap) {
-                                LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
-                                MarkerOptions options = new MarkerOptions().position(latLng).title("I'm here");
-                                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
-                                googleMap.addMarker(options);
-                            }
-                        });
-                    }
+            getCurrentLocation(task);
+        } else {
+            ActivityCompat.requestPermissions(Map.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+    }
+    
+    private void getCurrentLocation(Task<Location> task) {
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null){
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(@NonNull GoogleMap googleMap) {
+                            LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                            MarkerOptions options = new MarkerOptions().position(latLng).title("I'm here");
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                            googleMap.addMarker(options);
+                        }
+                    });
                 }
-            });
-        // }
+            }
+        });
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == 44) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getCurrentLocation();
+        if (requestCode == 44){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (ActivityCompat.checkSelfPermission(Map.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    Task<Location> task = user.getLastLocation();
+                    getCurrentLocation(task);
+                }
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }*/
-
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-
     }
+
 }
